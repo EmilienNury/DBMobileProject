04cd3b4d257d2fa8a085568340f04a0f8c6154e1
pirpr <quentin.pires@etu.univ-lyon1.fr>
2022-02-09 11:44:09 +0100

    Add LandmarkViewController

---
+++ BDMobileProject/Controllers/LandmarkViewController.swift
@@ -0,0 +1,18 @@
+//
+//  LandmarkViewController.swift
+//  BDMobileProject
+//
+//  Created by lpiem on 09/02/2022.
+//
+
+import UIKit
+
+class LandmarkViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
